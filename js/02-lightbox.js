89803abd350684712c7aa3eb2f6378350831cd09
Alex8804 <128159385+Alex8804@users.noreply.github.com>
2023-08-07 08:13:27 +0200

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const cardMarkup = createMarkup(galleryItems);
+
+const lightBoxOpt = {
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+};
+
+gallery.insertAdjacentHTML("afterbegin", cardMarkup);
+
+function createMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+   </li>`
+    )
+    .join("");
+}
+
+const simpleGallery = new SimpleLightbox(".gallery a", { ...lightBoxOpt });
